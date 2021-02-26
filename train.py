d6702537f3203c22927973ab199abc5a2a656461
Yiby Morales <yimp34@gmail.com>
2021-02-26 02:17:51 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+#pruueba del train.py
 import argparse
 import os
 import numpy as np
@@ -10,47 +11,27 @@ from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 from azureml.core import Dataset
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 #from sklearn.ensemble import RandomForestClassifier
 #from sklearn.datasets import make_classification
 #from sklearn.datasets import load_iris
 #from sklearn.model_selection import cross_val_score
 from sklearn.tree import DecisionTreeClassifier
-ds = Dataset.get_by_name(ws,'alcohol2')
-ds1=ds.to_pandas_dataframe()
+from sklearn.multioutput import MultiOutputClassifier
+ds = Dataset.get_by_name(ws,'alcohol')
 run = Run.get_context()
-#
-    # Dict for cleaning data
-#    months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
-#    weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
 
-    # Clean and one hot encode data
- #   x_df = data.to_pandas_dataframe().dropna()
- #   jobs = pd.get_dummies(x_df.job, prefix="job")
- #   x_df.drop("job", inplace=True, axis=1)
- #   x_df = x_df.join(jobs)
- #   x_df["marital"] = x_df.marital.apply(lambda s: 1 if s == "married" else 0)
- #   x_df["default"] = x_df.default.apply(lambda s: 1 if s == "yes" else 0)
- #   x_df["housing"] = x_df.housing.apply(lambda s: 1 if s == "yes" else 0)
- #   x_df["loan"] = x_df.loan.apply(lambda s: 1 if s == "yes" else 0)
- #   contact = pd.get_dummies(x_df.contact, prefix="contact")
- #   x_df.drop("contact", inplace=True, axis=1)
- #   x_df = x_df.join(contact)
- #   education = pd.get_dummies(x_df.education, prefix="education")
- #   x_df.drop("education", inplace=True, axis=1)
- #   x_df = x_df.join(education)
- #   x_df["month"] = x_df.month.map(months)
- #   x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
- #   x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
-def clean_data(data): 
-    x=data.to_pandas_dataframe
-
+def clean_data(dat): 
+    xd=dat.to_pandas_dataframe()
     schools = {"GP":1, "MS":2}
     sexs = {"F":1, "M":2}
+    addresss = {"U":1, "R":2}
     famsizes = {"LT3":1, "GT3":2}
     Pstatuss = {"A":1, "T":2}
     Mjobs = {"at_home":1, "services":2, "health":3, "teacher":4, "other":5}
     Fjobs = {"at_home":1, "services":2, "health":3, "teacher":4, "other":5}
+    guardians = {"mother":1, "father":2, "other":3}
     reasons = {"course":1, "home":2, "reputation":3, "other":4}
 
 
@@ -70,49 +51,43 @@ def clean_data(data):
  #   education = pd.get_dummies(x_df.education, prefix="education")
  #   x_df.drop("education", inplace=True, axis=1)
  #   x_df = x_df.join(education)
-    x["school"] = x.school.map(schools)
-    x["famsize"] = x.famsize.map(famsizes)
-    x["sex"] = x.sex.map(sexs)
-    x["Pstatus"] = x.Pstatus.map(Pstatuss)
-    x["Mjob"] = x.Mjob.map(Mjobs)
-    x["Fjob"] = x.Fjob.map(Fjobs)
-    x["reason"] = x.reason.map(reasons)
+    xd["school"] = xd.school.map(schools)
+    xd["address"] = xd.address.map(addresss)
+    xd["famsize"] = xd.famsize.map(famsizes)
+    xd["sex"] = xd.sex.map(sexs)
+    xd["Pstatus"] = xd.Pstatus.map(Pstatuss)
+    xd["Mjob"] = xd.Mjob.map(Mjobs)
+    xd["Fjob"] = xd.Fjob.map(Fjobs)
+    xd["reason"] = xd.reason.map(reasons)
+    xd["guardian"] = xd.guardian.map(guardians)
  #   x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
-    x["schoolsup"] = x.schoolsup.apply(lambda s: 1 if s == "yes" else 0)
-    x["famsup"] = x.famsup.apply(lambda s: 1 if s == "yes" else 0)
-    x["paid"] = x.paid.apply(lambda s: 1 if s == "yes" else 0)
-    x["activities"] = x.activities.apply(lambda s: 1 if s == "yes" else 0)
-    x["nursery"] = x.nursery.apply(lambda s: 1 if s == "yes" else 0)
-    x["higher"] = x.higher.apply(lambda s: 1 if s == "yes" else 0)
-    x["internet"] = x.internet.apply(lambda s: 1 if s == "yes" else 0)
-    x["romantic"] = x.romantic.apply(lambda s: 1 if s == "yes" else 0)
-return x
+    xd["schoolsup"] = xd.schoolsup.apply(lambda s: 1 if s == "yes" else 0)
+    xd["famsup"] = xd.famsup.apply(lambda s: 1 if s == "yes" else 0)
+    xd["paid"] = xd.paid.apply(lambda s: 1 if s == "yes" else 0)
+    xd["activities"] = xd.activities.apply(lambda s: 1 if s == "yes" else 0)
+    xd["nursery"] = xd.nursery.apply(lambda s: 1 if s == "yes" else 0)
+    xd["higher"] = xd.higher.apply(lambda s: 1 if s == "yes" else 0)
+    xd["internet"] = xd.internet.apply(lambda s: 1 if s == "yes" else 0)
+    xd["romantic"] = xd.romantic.apply(lambda s: 1 if s == "yes" else 0)
+    xd=xd.apply(pd.to_numeric, errors='coerce')
+    xd=xd.dropna()
+    xd=xd.reset_index(drop=True)
+    return xd
 
 def main():
 
     x = clean_data(ds)
-    y= x.drop("Walc", inplace=False, axis=1)
-
-    
- #   return x_df, y_df
-    
-    # Add arguments to script
-#x, y = clean_data(ds1)
-
-# TODO: Split data into train and test sets.
+    y= x.pop("Walc")
 
 
-    x_train, x_test = train_test_split(x, test_size=0.2)
-    y_train, y_test = train_test_split(y, test_size=0.2)
+    x_train, x_test = train_test_split(x, test_size=0.2,train_size=0.8,random_state=30)
+    y_train, y_test = train_test_split(y, test_size=0.2,train_size=0.8)
 
     parser = argparse.ArgumentParser()
-   #max_leaf_nodes
-    #parser.add_argument('--n_estimators', type=int, default=100, help="Inverse of regularization strength. Smaller values cause stronger regularization")
     parser.add_argument('--max_leaf_nodes', type=int, default=10, help="Inverse of regularization strength. Smaller values cause stronger regularization")
     parser.add_argument('--min_samples_split', type=int, default=2, help="Maximum number of iterations to converge")
 
     args = parser.parse_args()
-
     run.log("max_leaf_nodes:", np.int(args.max_leaf_nodes))
     run.log("min_samples_split", np.int(args.min_samples_split))
 
@@ -120,10 +95,13 @@ def main():
 
     #---this-one--model = RandomForestClassifier(n_estimators = args.n_estimators, min_samples_split = args.min_samples_split)
 
-    model = DecisionTreeClassifier(min_samples_split = args.min_samples_split, max_leaf_nodes = args.max_leaf_nodes).fit(x_train,y_train)
+    model = DecisionTreeClassifier(min_samples_split = 2, max_leaf_nodes = 10)
+    model.fit(x_train,y_train)
+#classifier = MultiOutputClassifier(model, n_jobs=-1)
+#classifier.fit(x_train,y_train)
   #model = RandomForestClassifier(n_estimators = args.n_estimators)
-
-    accuracy = model.score(x_test, y_test)
+    y_pred = model.predict(x_test)
+    accuracy = accuracy_score(y_pred, y_test)
     run.log("Accuracy", np.float(accuracy))
     os.makedirs('outputs',exist_ok=True)
     joblib.dump(model, 'outputs/model.joblib')
